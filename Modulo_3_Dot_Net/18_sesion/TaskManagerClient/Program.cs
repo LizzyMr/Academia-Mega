9b42d2be08fa12dea06f7ea90fa8b614ebc205d4
Lizeth Martínez <mimartinezro@megacable.com.mx>
2025-06-15 00:13:48 -0600

    Actividad - Sesión 18.

---
+++ Modulo_3_Dot_Net/18_sesion/TaskManagerClient/Program.cs
@@ -0,0 +1,31 @@
+using TaskManagerClient.Services;
+using TaskManagerClient;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
+var AllowedOrigin = "BlazorClient";
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(AllowedOrigin, policy =>
+    {
+        policy.WithOrigins("http://localhost:5030")
+            .AllowAnyHeader()
+            .AllowAnyMethod();
+            // .AllowCredentiads() // Solo si usamos una cookie de sesión
+    });
+});
+
+builder.RootComponents.Add<App>("#app");
+
+builder.Services.AddSingleton(sp =>
+{
+    var http = new HttpClient();
+    return http;
+});
+
+builder.Services.AddScoped<ITaskReader, TaskService>();
+builder.Services.AddScoped<ITaskWriter, TaskService>();
+
+
+await builder.Build().RunAsync();
