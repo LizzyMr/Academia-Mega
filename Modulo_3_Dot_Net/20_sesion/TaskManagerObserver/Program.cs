8281aefa890b891153ff302b9d789647dcd87aa7
Lizeth Martínez <mimartinezro@megacable.com.mx>
2025-06-16 02:40:05 -0600

    Actividad - Sesión 20.

---
+++ Modulo_3_Dot_Net/20_sesion/TaskManagerObserver/Program.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.SignalR.Client;
+using TaskManager.Shared.Events;
+
+
+var connection = new HubConnectionBuilder()
+    .WithUrl("http://localhost:5244/taskEvents") //Cambiar por mi puerto
+    .Build();
+
+connection.On<TaskEvent>("TaskEvent", ev =>
+{
+    Console.WriteLine($"{ev.EventName}: {ev.Payload.Title}");
+});
+
+await connection.StartAsync();
+Console.WriteLine("Observando eventos...");
+await Task.Delay(Timeout.Infinite); //-1 Es lo mismo que el Timeout.Infinite
