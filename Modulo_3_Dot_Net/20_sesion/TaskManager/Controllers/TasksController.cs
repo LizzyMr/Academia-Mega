6b33cb3c950511b1fa0155ab537fc69988f05270
Lizeth Martínez <mimartinezro@megacable.com.mx>
2025-06-16 02:38:49 -0600

    Actividad - Sesión 20.

---
+++ Modulo_3_Dot_Net/20_sesion/TaskManager/Controllers/TasksController.cs
@@ -0,0 +1,84 @@
+using Microsoft.AspNetCore.Mvc;
+using TaskManager.Shared.Domain;
+using TaskManager.Repositories;
+using TaskManager.Services;
+
+namespace TaskManager.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TasksController : ControllerBase
+    {
+        private readonly ITaskRepository _repo;
+        private readonly IEnumerable<INotificationService> _notifiers;
+
+        public TasksController(ITaskRepository repo, IEnumerable<INotificationService> notifiers)
+        {
+            _repo = repo;
+            _notifiers = notifiers;
+        }
+
+        // GET /api/tasks
+        [HttpGet]  
+        public async Task<IEnumerable<TaskItem>> GetAll() =>
+            await _repo.GetAllAsync(); 
+
+        // GET /api/tasks/{id}
+        [HttpGet("{id}")]
+        public async Task<ActionResult<TaskItem>> GetById(Guid id)
+        {
+            var task = await _repo.GetAsync(id);
+            if (task == null)
+                return NotFound();
+
+            return Ok(task);
+        }
+
+        // POST /api/tasks
+        [HttpPost]
+        public async Task<ActionResult<TaskItem>> Create(TaskItem newTask)
+        {
+            await _repo.AddAsync(newTask);
+
+            // Notificar a todos los servicios (email y sms)
+            foreach (var notifier in _notifiers)
+            {
+                await notifier.NotifyTaskCreatedAsync(newTask);
+            }
+
+            return CreatedAtAction(nameof(GetById), new { id = newTask.id }, newTask);
+        }
+
+        // PUT /api/tasks/{id}
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Update(Guid id, TaskItem updatedTask)
+        {
+            var existing = await _repo.GetAsync(id);
+            if (existing == null)
+                return NotFound();
+
+            // Actualizamos los campos necesarios
+            existing.Title = updatedTask.Title;
+            existing.Description = updatedTask.Description;
+
+            if (!existing.IsDone && updatedTask.IsDone)
+                existing.Complete();
+
+            await _repo.UpdateAsync(existing);
+
+            return NoContent();
+        }
+
+        // DELETE /api/tasks/{id}
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(Guid id)
+        {
+            var existing = await _repo.GetAsync(id);
+            if (existing == null)
+                return NotFound();
+
+            await _repo.DeleteAsync(id);
+            return NoContent();
+        }
+    }
+}
---
+++ Modulo_3_Dot_Net/20_sesion/TaskManager/Program.cs
@@ -0,0 +1,56 @@
+//using Microsoft.AspNetCore.Mvc;
+using TaskManager.Repositories;
+using TaskManager.Services;
+using TaskManager.Shared.PubSub;
+using TaskManager.Hubs;
+//using Microsoft.Extensions.DependencyInjection;
+
+var builder = WebApplication.CreateBuilder(args);
+
+var AllowedOrigin = "BlazorClient";
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(AllowedOrigin, policy =>
+    {
+        policy.WithOrigins("http://localhost:5030")
+            .AllowAnyHeader()
+            .AllowAnyMethod();
+            // .AllowCredentiads() // Solo si usamos una cookie de sesión
+    });
+});
+
+// Servicios
+builder.Services.AddControllers();
+builder.Services.AddScoped<INotificationService, EmailNotificationService>();
+builder.Services.AddScoped<INotificationService, SmsNotificationService>();
+builder.Services.AddSingleton<ITaskRepository, InMemoryTaskRepository>();
+
+builder.Services.AddSingleton<EventBus>();
+builder.Services.AddSignalR();
+
+//builder.Services.AddScoped<ITaskRepository, InMemoryTaskRepository>();
+
+
+// Swagger
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Middleware
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseCors(AllowedOrigin);
+
+app.UseHttpsRedirection();
+app.UseCors();
+app.UseAuthorization();
+app.MapControllers();
+
+app.MapHub<TaskEventHub>("/taskEvents");
+
+app.Run();
